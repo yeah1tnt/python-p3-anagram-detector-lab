afcc90acb146bb853d7cee6f01907698284de970
Tong Tang <pichip22@hotmail.com>
2023-07-14 22:55:51 -0400

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.wordSorted = sorted(i for i in word)
+
+    def match(self, anagram):
+        list = []
+        for i in anagram:
+            if sorted(j for j in i) == self.wordSorted:
+                list.append(i)
+        return list
